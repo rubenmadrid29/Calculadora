21a63f7a80620b96037800ad22de398b9ade4129
NURIA <NURIA@W8>
2020-04-12 13:12:04 +0200

    Comentario para Entornos

---
+++ proyecto1/src/proyecto1/Calculadora.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+/**
+ *
+ * @author NURIA
+ */
+public class Calculadora {
+
+private int num1;
+private int num2;
+
+public Calculadora (int a, int b){
+num1=a;
+num2=b;
+}
+
+public int suma(){
+int result=num1+num2;
+return result;
+}
+
+public int resta(){
+int result= num1-num2;
+return result;
+}
+
+public int multiplicar(){
+int result= num1*num2;
+return result;
+}
+
+public int division(){
+int result= num1/num2;
+return result;
+}
+}
+
